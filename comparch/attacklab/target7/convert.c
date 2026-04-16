57c8f6ffc6944206764727ca2f28268639d9830e
Raul-Andrei POP <raul-andrei.pop@polytechnique.edu>
2026-04-16 13:34:40 +0200

    comparch attacklab

--- comparch/attacklab/target7/convert.c
+++ comparch/attacklab/target7/convert.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
+int hexmatch(unsigned val, char *sval)
+{
+    char cbuf[110];
+    /* Make position of check string unpredictable */
+    char *s = cbuf + random() % 100;
+    sprintf(s, "%.8x", val);
+    return strncmp(sval, s, 9) == 0;
+}
 
 int main() {
 
-    printf("plm: \x13\x30\x40\x00\x00\x00\x00\x00");
-    
-
+    printf("%d\n", hexmatch(0xdeadbeef, "deadbeef"));
 }
---
+++ comparch/attacklab/target7/cookie_to_s.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    char cookie[] = "2d274378";
+
+    for (auto c : cookie) {
+        printf("%x ", c);
+    }
+}
